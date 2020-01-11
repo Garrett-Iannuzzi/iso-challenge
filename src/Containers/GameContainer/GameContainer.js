992f18e91b37c7b0cc31de1b85cb6e5fb87dfd02
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-11 15:25:15 -0700

    Add name and skill display in gameContainer

--- src/Containers/GameContainer/GameContainer.js
+++ src/Containers/GameContainer/GameContainer.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 
 const GameContainer = (props) => {
 
+  const { playerInfo, teamInfo } = props;
+
   const doIt = (position) => {
     return(
       <>
@@ -14,7 +16,7 @@ const GameContainer = (props) => {
   }
 
   const getPlayersByPosition = (position) => {
-    const filteredPlayers = props.playerInfo.filter(player => player.position === position);
+    const filteredPlayers = playerInfo.filter(player => player.position === position);
     const filteredNames = filteredPlayers.reduce((fullName, currentPlayer) => {
       fullName += currentPlayer.first_name + ' ' + currentPlayer.last_name
       return fullName
@@ -26,12 +28,22 @@ const GameContainer = (props) => {
     )
   }
 
+  const getTeamMetric = (id, metric) => {
+    const teamOne = teamInfo[0]
+    const teamTwo = teamInfo[1]
+    if(teamOne.id === id) {
+      return teamOne[metric]
+    } else {
+      return teamTwo[metric]
+    }
+  }
+
   return(
     <>
       <div className='game-container'>
         <section className='section-team'>
-          <h2>Team Name</h2>
-          <h4>Skill Level</h4>
+          <h2>{getTeamMetric(1, 'teamOneName')}</h2>
+          <h4>{getTeamMetric(1, 'skillLevelOne')}</h4>
             {doIt('Center')}
             {getPlayersByPosition('C')}
 
@@ -48,8 +60,8 @@ const GameContainer = (props) => {
             {getPlayersByPosition('F')}
         </section>
         <section className='section-team'>
-          <h2>Team Name</h2>
-          <h4>Skill Level</h4>
+          <h2>{getTeamMetric(2, 'teamTwoName')}</h2>
+          <h4>{getTeamMetric(2, 'skillLevelTwo')}</h4>
             {doIt('Center')}
             {getPlayersByPosition('C')}
 
@@ -72,7 +84,8 @@ const GameContainer = (props) => {
 }
 
 export const mapState = state => ({
-  playerInfo: state.playerInfo
+  playerInfo: state.playerInfo,
+  teamInfo: state.teamInfo
 })
 
 export default connect(mapState)(GameContainer)
