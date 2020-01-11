44aee5dc5c1dda4cc81977f944ead2d94a36b191
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-10 23:02:28 -0700

    Add testing for mapDispatchToProps in App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,7 +1,7 @@
-
 import React from 'react';
-import { App } from './App';
+import { App, mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
+import { getPlayerInfo } from '../../actions/actions';
 
 describe('App', () => {
 
@@ -16,6 +16,57 @@ describe('App', () => {
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot()
     });
+  });
+
+  describe('mapDispatchToProps', () => {
+
+    it('Should call mapDispatchToProps with correct information info when playerInfo is called', () => {
+      const mockDispatch = jest.fn();
+      const mockPlayerData = [
+        {        
+          id:14,
+          first_name:"Ike",
+          height_feet:null,
+          height_inches:null,
+          last_name:"Anigbogu",
+          position:"C",
+          team: {
+            id:12,
+            abbreviation:"IND",
+            city:"Indiana",
+            conference:"East",
+            division:"Central",
+            full_name:"Indiana Pacers",
+            name:"Pacers"
+          },
+          weight_pounds:null
+        },
+        {
+          id:25,
+          first_name:"Ron",
+          height_feet:null,
+          height_inches:null,
+          last_name:"Baker",
+          position:"G",
+          team: {
+            id:20,
+            abbreviation:"NYK",
+            city:"New York",
+            conference:"East",
+            division:"Atlantic",
+            full_name:"New York Knicks",
+            name:"Knicks"
+          },
+          weight_pounds:null
+        }
+      ]
+      const actionToDispatch = getPlayerInfo(mockPlayerData);
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.playerInfo(mockPlayerData);
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
   });
 
 })
