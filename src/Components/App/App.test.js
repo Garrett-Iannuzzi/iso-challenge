5b08a0a1e1f3578e6317ede7ae76f6c76d5b3ad0
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-10 22:24:33 -0700

    Add snapshot testing for app

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import { App } from './App';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+
+  let wrapper
+
+  describe('App Component', () => {
+
+    beforeEach(() => {
+      wrapper = shallow(<App />)
+    });
+
+    it('should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    });
+  });
+
+})
