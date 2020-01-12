02fbcc10c09e7b24377eccde2a8d5b1a894c6b7a
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-11 22:42:05 -0700

    Add testing for homeContainer mapState and btn click

--- src/Containers/HomeContainer/HomeContainer.test.js
+++ src/Containers/HomeContainer/HomeContainer.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { HomeContainer, mapDispatchToProps } from './HomeContainer';
+import { HomeContainer, mapDispatchToProps, mapStateToProps } from './HomeContainer';
 import { shallow } from 'enzyme';
-import { teamInfo, getTeamsInfo } from '../../actions/actions';
+import { teamInfo, getTeamsInfo, isLoggedIn } from '../../actions/actions';
 
 describe('HomeContainer', () => {
 
@@ -12,11 +12,12 @@ describe('HomeContainer', () => {
     beforeEach(() => {
     wrapper = shallow(<HomeContainer
       teamInfo={jest.fn()}
+      isLoggedIn={jest.fn()}
     />)
     });
 
     it('should match the snapshot', () => {
-      expect(wrapper).toMatchSnapshot()
+      expect(wrapper).toMatchSnapshot();
     });
 
     it('Should setState when handleChange is called', () => {
@@ -24,7 +25,7 @@ describe('HomeContainer', () => {
       mockEvent = { target: {name: 'teamOneName', value:'Bazanga'} }
       wrapper.instance().handleChange(mockEvent);
 
-      expect(wrapper.state()).toEqual({ teamOneName: 'Bazanga', teamTwoName: '', skillLevelOne: '', skillLevelTwo: '' })
+      expect(wrapper.state()).toEqual({ teamOneName: 'Bazanga', teamTwoName: '', skillLevelOne: '', skillLevelTwo: '' });
     });
 
     it('Should call handleChange when teamOneName input is changed', () => {
@@ -33,7 +34,7 @@ describe('HomeContainer', () => {
 
       wrapper.find('.input-team-one').simulate('change', mockEvent);
 
-      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent)
+      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent);
     });
 
     it('Should call handleChange when teamTwoName input is changed', () => {
@@ -51,7 +52,7 @@ describe('HomeContainer', () => {
 
       wrapper.find('.select-skill-one').simulate('change', mockEvent);
 
-      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent)
+      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent);
     });
 
     it('Should call handleChange when player two selects skill level', () => {
@@ -60,8 +61,39 @@ describe('HomeContainer', () => {
 
       wrapper.find('.select-skill-two').simulate('change', mockEvent);
 
-      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent)
+      expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent);
     });
+
+    it('Should call handleStartError when the button is clicked', () => {
+      wrapper.instance().handleStartError = jest.fn();
+
+      wrapper.find('.btn-start').simulate('click');
+
+      expect(wrapper.instance().handleStartError).toHaveBeenCalled();
+    });
+
+    it('Should call submitTeams when the button is clicked', () => {
+      wrapper.instance().submitTeams = jest.fn();
+
+      wrapper.find('.btn-start').simulate('click');
+      wrapper.instance().submitTeams()
+
+      expect(wrapper.instance().submitTeams).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('mapStateToProps', () => {
+
+    it('Should return a login value of true', () => {
+      const mockState = {
+        isLoggedIn: true,
+      };
+      const expected = { login: true };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    })
   });
 
   describe('mapDispatchToProps', () => {
@@ -74,7 +106,17 @@ describe('HomeContainer', () => {
       mappedProps.teamInfo({ teamInfo: [{ teamOneName: 'G-Swish', skillLevelOne: 'NBA' }, { teamTwoName: 'D-Buckets', skillLevelTwo: 'NBA' }] });
 
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-    })
+    });
+
+    it('Should call mapDispatchToProps with correct loggedIn info when handleStartError is called', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = isLoggedIn({ isLoggedIn: false });
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.isLoggedIn({ isLoggedIn: false });
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 
 })
