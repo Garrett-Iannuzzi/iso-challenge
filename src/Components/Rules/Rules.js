45876c74871946bfc0e96aea56bf8c464aeeea6f
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-13 00:24:06 -0700

    Add style for rules component

--- src/Components/Rules/Rules.js
+++ src/Components/Rules/Rules.js
@@ -8,11 +8,11 @@ const Rules = () => {
       <ul>
         <li>Choose a team name & skill level</li>
         <li>Choose a player for each position from the list</li>
-        <li>Draft style: a player can only appear on one team</li>
+        <li><b>Draft style:</b> a player can only appear on one team</li>
         <li>Click submit when both teams are full</li>
-        <li>Iso-Challenge will unlock all game stats for your players</li>
+        <li><b>Iso-Challenge</b> will unlock all game stats for your players</li>
         <li>Argue away ay which team has the better stats</li>
-        <li>NO FIGHTING</li>
+        <li> &#128170; NO FIGHTING &#128170;</li>
       </ul>
     </article>
   )
