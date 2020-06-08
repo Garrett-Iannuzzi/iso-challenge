02570d3b30e644acb3ae9aaccbdf6b86178bcadc
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-06-08 14:41:35 -0600

    Remove style var from button element

--- src/Components/Elements/Button.js
+++ src/Components/Elements/Button.js
@@ -2,10 +2,6 @@ import React from 'react';
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types';
 
-const buttonStyle = styled.button({
-  backgroundColor: 'red'
-})
-
 
 export const Button = ({ btnInfo:{ name, fn, className } }) => {
   return(
